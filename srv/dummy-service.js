880776c0ede923850db992da227935613efa2dfa
Peter Gilberg <peter.gilberg@orbis.de>
2024-01-11 17:39:14 +0000

    Adjustments

--- srv/dummy-service.js
+++ srv/dummy-service.js
@@ -4,12 +4,20 @@ class DummyService extends cds.ApplicationService {
 
     this.on("READ", "DummyEntity", async (req) => {
 
-      return [
+      if (!req.user.is('SandboxMember')) {
 
-        { someNumber: 1, someText: "The first rule of Fight Club is: you do not talk about Fight Club" },
-        { someNumber: 2, someText: "The second rule of Fight Club is: you DO NOT talk about Fight Club! " },
-        { someNumber: 2, someText: "Third rule of Fight Club: someone yells 'stop!', goes limp, taps out, the fight is over." }]
+        return [{ someNumer: 99, someText: `Sorry ${req.user.id}. You're not a member.` }];
 
+      } else {
+
+        return [
+          { someNumber: 1, someText: `Welcome ${req.user.id}. These are the rules of the Fight Club.` },
+          { someNumber: 1, someText: "The first rule of Fight Club is: you do not talk about Fight Club" },
+          { someNumber: 2, someText: "The second rule of Fight Club is: you DO NOT talk about Fight Club! " },
+          { someNumber: 2, someText: "Third rule of Fight Club: someone yells 'stop!', goes limp, taps out, the fight is over." }]
+
+      }
+    
     });
 
   }
